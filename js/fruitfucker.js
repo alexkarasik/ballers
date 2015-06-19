11f12e97b7b5d862a2b33bf57361703034e77271
kinkorea <alexkarasik89@gmail.com>
2015-06-19 17:17:31 +0900

    fuck you pay me

--- js/fruitfucker.js
+++ js/fruitfucker.js
@@ -1,1 +1,13 @@
-var fruits = ['http://www.arthursclipart.org/fruits/fruit/apple%208.gif']
+$(document).ready(function() {
+    var fruitUrls = ['http://www.naturipefarms.com/wp-content/uploads/2012/04/Blueberry.png', 
+                     'https://www.organicfacts.net/wp-content/uploads/2013/05/Banana21.jpg?a1d5ed',
+                     'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/PerfectStrawberry.jpg/220px-PerfectStrawberry.jpg',
+                     'http://pngimg.com/upload/kiwi_PNG4035.png']
+  $('button').click(function(e) {
+    
+    var fruitUrl = fruitUrls.shift()
+    console.log(fruitUrls)
+    
+    $('img').attr('src', fruitUrl)
+  })
+})
